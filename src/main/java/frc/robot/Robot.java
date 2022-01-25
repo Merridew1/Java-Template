6974b97b21c7feca55875be0faab3ffbd98945e8
Alex Resnick <adr8292@gmail.com>
2022-01-25 08:50:12 -0600

    Update formatter (#6)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,49 +14,49 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
-    System.out.println("Robot Init");
-  }
+    @Override
+    public void robotInit() {
+        System.out.println("Robot Init");
+    }
 
-  @Override
-  public void robotPeriodic() {
-    System.out.println("Robot Periodic");
-  }
+    @Override
+    public void robotPeriodic() {
+        System.out.println("Robot Periodic");
+    }
 
-  @Override
-  public void autonomousInit() {
-    System.out.println("Auto Init");
-  }
+    @Override
+    public void autonomousInit() {
+        System.out.println("Auto Init");
+    }
 
-  @Override
-  public void autonomousPeriodic() {
-    System.out.println("Auto Periodic");
-  }
+    @Override
+    public void autonomousPeriodic() {
+        System.out.println("Auto Periodic");
+    }
 
-  @Override
-  public void teleopInit() {
-    System.out.println("Teleop Init");
-  }
+    @Override
+    public void teleopInit() {
+        System.out.println("Teleop Init");
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    System.out.println("Teleop Periodic");
-  }
+    @Override
+    public void teleopPeriodic() {
+        System.out.println("Teleop Periodic");
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    /** This function is called once when the robot is disabled. */
+    @Override
+    public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    /** This function is called periodically when disabled. */
+    @Override
+    public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+    /** This function is called once when test mode is enabled. */
+    @Override
+    public void testInit() {}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
 }
